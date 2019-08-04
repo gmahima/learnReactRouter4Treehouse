ff935423b90b8e4e71215d6ee5ad17c098e02eba
Mahima Gangavarapu <39587007+gmahima@users.noreply.github.com>
2019-08-04 12:05:04 +0530

    add route

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
-
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./Home";
 const App = () => (
-  <div className="container">
-
-  </div>
+  <BrowserRouter>
+    <div className="container">
+      <Route path="/" component={Home} />
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
