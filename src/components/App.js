58749947e4c74fdb3929feaf24ff47b42f3193ad
Mahima Gangavarapu <39587007+gmahima@users.noreply.github.com>
2019-08-04 12:15:07 +0530

    add routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./Home";
+import Header from "./Header";
+import About from "./About";
 const App = () => (
   <BrowserRouter>
     <div className="container">
-      <Route path="/" component={Home} />
+      <Header />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
     </div>
   </BrowserRouter>
 );
