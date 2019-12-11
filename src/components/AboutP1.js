ed9a80cd4d97b62d8ecf787dd959ea8b3cd13612
Mahima Gangavarapu <39587007+gmahima@users.noreply.github.com>
2019-12-11 18:36:33 +0530

    using component instead of render

---
+++ src/components/AboutP1.js
@@ -0,0 +1,6 @@
+import React from "react";
+import About from "./About";
+export default function AboutP1() {
+  console.log("hi");
+  return <About title="hjgugy" />;
+}
--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./Home";
 import Header from "./Header";
-import About from "./About";
+
+import AboutP1 from "./AboutP1";
 import Courses from "./Courses";
 import Teachers from "./Teachers";
 import Featured from "./Featured";
@@ -16,8 +17,8 @@ const App = () => (
         <Route path="/courses" component={Courses} />
         <Route exact path="/teachers" component={Teachers} />
         <Route path="/teachers/:course/:name" component={Featured} />
-        <Route path="/about" render={() => <About title="About this site" />} />
-        <Route render={() => <NotFound />} />
+        <Route path="/about" component={AboutP1} />
+        <Route render={NotFound} />
       </Switch>
     </div>
   </BrowserRouter>
